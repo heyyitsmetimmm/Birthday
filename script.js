f67c3e575ccdf51ebf357e9d3c98b12274b3ade0
heyyitsmetimmm <zhiyan0930@gmail.com>
2026-01-13 16:24:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const recipientName = "Ru Xing"; // Customizable Name
-    const messageGreeting = "Happy Birthday"; // Customizable Greeting
+    const messageGreeting = "Happy Birthday!!!"; // Customizable Greeting
 
     // --- Utility Function to Transition Steps ---
     function transitionToStep(targetStepId) {
